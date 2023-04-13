2c65a850b839d4232c2b807c197e3d38680a88ae
AlexTereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2023-04-13 21:42:17 +0100

    Move test directory

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -4,7 +4,15 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Greeting {
     public static int sayHi() {
-        System.out.println(StringUtils.capitalize("hello, world!"));
+        System.out.println(StringUtils.capitalize("hello, " + Greeting.getWorld(0)));
         return 0;
     }
+    /* Using Java 17 syntax. */
+    private static String getWorld(int value) {
+        String result = switch (Math.abs(value)) {
+            case 0 -> "world";        
+            default -> "World";
+        };
+        return result;
+}
 }
